d55d4bac070817b36e0ea4b3120af21f6982d07e
leonard <leonardloh08@gmail.com>
2024-09-13 01:09:01 +0800

    Feedback UI and partial function

---
+++ app/controllers/FeedbackHistory.php
@@ -0,0 +1,16 @@
+<?php
+
+class FeedbackHistory
+{
+    use Controller;
+
+    public function index()
+    {
+        $data = null;
+        echo "<script>console.log('testingjwhbf');</script>";
+        //Route to the destinaiton page, with passing data from the Model
+        $this->view('Customer/Feedback/FeedbackHistory', $data);
+    }
+}
+
+
