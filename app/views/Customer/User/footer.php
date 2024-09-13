88674fc58671863d1448655f8a2c8a90becc8289
kyanchong <kahyanchong0803@gmail.com>
2024-09-13 13:55:16 +0800

    include footer, header and navigation

---
+++ app/views/Customer/User/footer.php
@@ -0,0 +1,8 @@
+<!--Footer-->
+<footer id="footer">
+    <div class="footer-container container-fluid">
+        <div class="bottom" style="text-align: center; color: white;">
+            © 2023 - 2024 DreamWorks Cinema (Copyright All Right Reserved)
+        </div>
+    </div>
+</footer>
