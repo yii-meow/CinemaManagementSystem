3fdba9236cb4e3271b588fd0005862ea22f8cdb7
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2024-09-20 14:24:12 +0800

    Update navigationBar.php

--- app/views/navigationBar.php
+++ app/views/navigationBar.php
@@ -1,4 +1,79 @@
 <!--Navigation Bar-->
-<div class="dropdowns" style="display: flex; flex-flow: row nowrap; min-height: 20px;">
-
+<div class="dropdowns" style="display: flex; flex-flow: row nowrap;">
+    <div class="drop">
+        <button class="btn dropdown-toggle dropbtn" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+            NEW MOVIES
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="">New Action</a></li>
+            <li><a class="dropdown-item" href="">New Horror</a></li>
+            <li><a class="dropdown-item" href="">New Animation</a></li>
+            <li><a class="dropdown-item" href="">New Romance</a></li>
+        </ul>
+    </div>
+    <div class="drop">
+        <button class="btn dropdown-toggle dropbtn" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+            ACTION
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="">Superhero</a></li>
+            <li><a class="dropdown-item" href="">War</a></li>
+            <li><a class="dropdown-item" href="">Adventure</a></li>
+        </ul>
+    </div>
+    <div class="drop">
+        <button class="btn dropdown-toggle dropbtn" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+            HORROR
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="">Supernatural Horror</a></li>
+            <li><a class="dropdown-item" href="">Zombie</a></li>
+        </ul>
+    </div>
+    <div class="drop">
+        <button class="btn dropdown-toggle dropbtn" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+            ANIMATION
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="">2D Animation</a></li>
+            <li><a class="dropdown-item" href="">3D Animation</a></li>
+            <li><a class="dropdown-item" href="">Anime</a></li>
+        </ul>
+    </div>
+    <div class="drop">
+        <button class="btn dropdown-toggle dropbtn" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+            ROMANCE
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="">Romantic Comedy</a></li>
+            <li><a class="dropdown-item" href="">Romantic Drama</a></li>
+        </ul>
+    </div>
+    <ul class="nav-item dropdown">
+        <button class="btn dropbtn" type="button" aria-expanded="false">
+            <a href="" style="text-decoration: none;"> NOW SHOWING</a>
+        </button>
+    </ul>
+    <ul class="nav-item dropdown">
+        <button class="btn dropbtn" type="button" aria-expanded="false">
+            <a href="" style="text-decoration: none;"> COMING SOON</a>
+        </button>
+    </ul>
+    <div class="drop">
+        <button class="btn dropdown-toggle dropbtn" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+            FORUM
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="<?=ROOT?>/Forum/index">Community Forum</a></li>
+            <li><a class="dropdown-item" href="<?=ROOT?>/AddPost/index">Create Post</a></li>
+            <li><a class="dropdown-item" href="<?=ROOT?>/MyPost/index">My Post</a></li>
+            <li><a class="dropdown-item" href="<?=ROOT?>/ShowLikedPost/index">Liked Post</a></li>
+        </ul>
+    </div>
 </div>
