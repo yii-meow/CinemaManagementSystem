7e5f4007e9e229a89d4e559dadd705e244582e7b
kyanchong <kahyanchong0803@gmail.com>
2024-09-22 13:54:23 +0800

    add name

--- app/models/Admin.php
+++ app/models/Admin.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Author: Chong Kah Yan
+ */
 namespace App\models;
 
 use Doctrine\ORM\Mapping as ORM;
--- app/models/User.php
+++ app/models/User.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Author: Chong Kah Yan
+ */
 namespace App\models;
 
 use App\Factory\UserType;
